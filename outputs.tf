6ccda63959f35b05f2abdf77e14f25eaa28a9c97
ethanjenkins1 <ethanjenkins@microsoft.com>
2024-04-26 11:39:23 -0500

    Resolve linting issues

--- outputs.tf
+++ outputs.tf
@@ -7,5 +7,5 @@ output "private_endpoints" {
 # https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
 output "resource" {
   description = "This is the full output for the resource."
-  value       = azurerm_resource_group.TODO # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
+  value       = azurerm_batch_account.this
 }
