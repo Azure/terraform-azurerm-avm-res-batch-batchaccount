3d8ec9a76b23733a8741cc20850169c977fe096b
ethanjenkins1 <ethanjenkins@microsoft.com>
2024-08-15 14:11:18 -0500

    rollback changes

--- main.privateendpoint.tf
+++ main.privateendpoint.tf
@@ -1,9 +1,9 @@
-# The PE resource when we are managing the private_dns_zone_group block:
-resource "azurerm_private_endpoint" "this" {
-  for_each = { for k, v in var.private_endpoints : k => v if var.private_endpoints_manage_dns_zone_group }
 
-  location                      = each.value.location != null ? each.value.location : var.location
-  name                          = each.value.name != null ? each.value.name : "pep-${var.name}"
+resource "azurerm_private_endpoint" "this" {
+  for_each = var.private_endpoints
+
+  location                      = coalesce(each.value.location, var.location, )
+  name                          = each.value.name != null ? each.value.name : "pe-${var.name}"
   resource_group_name           = each.value.resource_group_name != null ? each.value.resource_group_name : var.resource_group_name
   subnet_id                     = each.value.subnet_resource_id
   custom_network_interface_name = each.value.network_interface_name
@@ -12,8 +12,8 @@ resource "azurerm_private_endpoint" "this" {
   private_service_connection {
     is_manual_connection           = false
     name                           = each.value.private_service_connection_name != null ? each.value.private_service_connection_name : "pse-${var.name}"
-    private_connection_resource_id = azurerm_key_vault.this.id
-    subresource_names              = ["MYSERVICE"] # map to each.value.subresource_name if there are multiple services.
+    private_connection_resource_id = azurerm_batch_account.this.id
+    subresource_names              = ["batchAccount", "nodeManagement"]
   }
   dynamic "ip_configuration" {
     for_each = each.value.ip_configurations
@@ -21,8 +21,8 @@ resource "azurerm_private_endpoint" "this" {
     content {
       name               = ip_configuration.value.name
       private_ip_address = ip_configuration.value.private_ip_address
-      member_name        = "MYSERVICE" # map to each.value.subresource_name if there are multiple services.
-      subresource_name   = "MYSERVICE" # map to each.value.subresource_name if there are multiple services.
+      member_name        = "sites"
+      subresource_name   = "sites"
     }
   }
   dynamic "private_dns_zone_group" {
@@ -35,33 +35,9 @@ resource "azurerm_private_endpoint" "this" {
   }
 }
 
-# Private endpoint application security group associations.
-# We merge the nested maps from private endpoints and application security group associations into a single map.
-locals {
-  private_endpoint_application_security_group_associations = { for assoc in flatten([
-    for pe_k, pe_v in var.private_endpoints : [
-      for asg_k, asg_v in pe_v.application_security_group_associations : {
-        asg_key         = asg_k
-        pe_key          = pe_k
-        asg_resource_id = asg_v
-      }
-    ]
-  ]) : "${assoc.pe_key}-${assoc.asg_key}" => assoc }
-}
-
 resource "azurerm_private_endpoint_application_security_group_association" "this" {
   for_each = local.private_endpoint_application_security_group_associations
 
   application_security_group_id = each.value.asg_resource_id
   private_endpoint_id           = azurerm_private_endpoint.this[each.value.pe_key].id
-}
-
-# You need an additional resource when not managing private_dns_zone_group with this module:
-
-# In your output you need to select the correct resource based on the value of var.private_endpoints_manage_dns_zone_group:
-output "private_endpoints" {
-  value       = var.private_endpoints_manage_dns_zone_group ? azurerm_private_endpoint.this_managed_dns_zone_groups : azurerm_private_endpoint.this_unmanaged_dns_zone_groups
-  description = <<DESCRIPTION
-A map of the private endpoints created.
-DESCRIPTION
 }
